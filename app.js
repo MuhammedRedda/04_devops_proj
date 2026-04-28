29bda5d0875a063d69f8ff195d3556c32800612c
MuhammedRedda <muhmmedredda@gmail.com>
2026-04-28 18:03:11 +0300

    update app.js and add index.html

--- app.js
+++ app.js
@@ -1,34 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.json());
+app.use(express.static(path.join(__dirname)));
 
-// Routes
-app.get("/", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: "🚀 DevOps App is running!",
-    timestamp: new Date()
-  });
-});
-
-app.get("/health", (req, res) => {
-  res.status(200).json({
-    status: "UP"
-  });
-});
-
-// 404 handler
-app.use((req, res) => {
-  res.status(404).json({
-    error: "Not Found"
-  });
-});
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
